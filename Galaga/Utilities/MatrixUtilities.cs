d6c911d396d7f01b9f83d77fd3af1e9e66382a5a
Alfredo <aoltra@uhurulabs.com>
2016-01-31 17:35:30 +0100

    Utilidades
    Utilidades para el cálculo de trayectorias

---
+++ Galaga/Utilities/MatrixUtilities.cs
@@ -0,0 +1,205 @@
+﻿#region GPL License
+/*  Galaga-SFML.Net: Galaga's Clon for educational purposes made with SFML.Net library
+    Copyright (C) 2015-2016  Alfredo Oltra. 
+   
+    This program comes with ABSOLUTELY NO WARRANTY.
+   
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+  
+    --------------------------------------------------------------------------------
+ 
+    You can contact the author by email: aoltra@uhurulabs.com, aoltra@gmail.com
+    or you can follow on Twitter: @aoltra
+*/
+#endregion
+
+using System;
+
+namespace edu.CiclosFormativos.DAM.DI.Galaga.Utilities
+{
+    /// <summary>
+    /// Encapsula las utilidades necesarias para la resolución de SLE (sistemas de ecuaciones lineales)
+    /// </summary>
+    /// <remarks>
+    /// Basado en las ideas del artículo de James McCaffrey
+    /// https://msdn.microsoft.com/de-de/magazine/jj863137.aspx 
+    /// </remarks>
+    class MatrixUtilities
+    {
+        /// <summary>
+        /// Soluciona un sistem de ecuaciones lineales
+        /// </summary>
+        /// <param name="A">Matriz de coeficientes <b>A</b> de <i>Ax=b</i></param>
+        /// <param name="b">Vector <b>b</b> de <i>Ax=b</i></param>
+        /// <returns>Vector de soluciones</returns>
+        public static double[] SystemSolve(double[][] A, double[] b)
+        {
+            int numElem = A.Length;             // número de elementos de la matriz
+            int[] perm;                        
+            int toggle;                          
+            double[][] luMatrix;
+
+            luMatrix = MatrixDecompose(A, out perm, out toggle);
+            if (luMatrix == null)
+                return null;                        
+            
+            double[] bp = new double[b.Length];
+            for (int i = 0; i < numElem; ++i)
+                bp[i] = b[perm[i]];
+
+            double[] x = HelperSolve(luMatrix, bp);
+            
+            return x;
+        }
+
+        /// <summary>
+        /// Descomposicion LU
+        /// </summary>
+        /// <param name="matrix">Matriz a descomponer</param>
+        /// <param name="perm"></param>
+        /// <param name="toggle"></param>
+        /// <returns>Matriz descompuesta</returns>
+        static double[][] MatrixDecompose(double[][] matrix, out int[] perm, out int toggle)
+        {
+            int n = matrix.Length;                          
+            double[][] result = MatrixDuplicate(matrix);
+            perm = new int[n];
+
+            for (int i = 0; i < n; ++i) { perm[i] = i; }
+            
+            toggle = 1;
+            for (int j = 0; j < n - 1; ++j)                     // columnas
+            {
+                double colMax = Math.Abs(result[j][j]);         // valor más alto en la columna j
+                int pRow = j;
+                for (int i = j + 1; i < n; ++i)
+                {
+                    if (result[i][j] > colMax)
+                    {
+                        colMax = result[i][j];
+                        pRow = i;
+                    }
+                }
+
+                if (pRow != j)                                  // cambia las filas
+                {
+                    double[] rowPtr = result[pRow];
+                    result[pRow] = result[j];
+                    result[j] = rowPtr;
+                    int tmp = perm[pRow];                       
+                    perm[pRow] = perm[j];
+                    perm[j] = tmp;
+                    toggle = -toggle; 
+                }
+                if (Math.Abs(result[j][j]) < 1.0E-20)       // cero
+                    return null; 
+
+                for (int i = j + 1; i < n; ++i)
+                {
+                    result[i][j] /= result[j][j];
+                    for (int k = j + 1; k < n; ++k)
+                        result[i][k] -= result[i][j] * result[j][k];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Duplica una matriz
+        /// </summary>
+        /// <param name="matrix">Matriz en formato array de punteros [][]</param>
+        /// <returns>Matriz duplicada en formato [][] </returns>
+        static double[][] MatrixDuplicate(double[][] matrix)
+        {
+            double[][] result = MatrixCreate(matrix.Length, matrix[0].Length);
+
+            for (int i = 0; i < matrix.Length; ++i)             // copia los valores
+                for (int j = 0; j < matrix[i].Length; ++j)
+                    result[i][j] = matrix[i][j];
+
+            return result;
+        }
+
+        /// <summary>
+        /// Crea una matriz de NxM, inicializada a ceros, en formato array de punteros [][]
+        /// </summary>
+        /// <param name="rows">número de filas</param>
+        /// <param name="cols">número de columnas</param>
+        /// <returns>Matriz creada en formato [][]</returns>
+        public static double[][] MatrixCreate(int rows, int cols)
+        {
+            double[][] result = new double[rows][];
+
+            for (int i = 0; i < rows; ++i)
+                result[i] = new double[cols];               // se inicializa automaticamente con 0.0
+            
+            return result;
+        }
+
+        /// <summary>
+        /// Calcula el determinante de una matriz 
+        /// </summary>
+        /// <param name="matrix">Matriz en formato de array de punteros [][]</param>
+        /// <returns>Determinante de la matriz</returns>
+        public static double MatrixDeterminant(double[][] matrix)
+        {
+            int[] perm;
+            int toggle;
+
+            double[][] lum = MatrixDecompose(matrix, out perm, out toggle);
+            if (lum == null)
+                throw new Exception("No es posible calcular el determinante");
+            
+            double result = toggle;
+            for (int i = 0; i < lum.Length; ++i)
+                result *= lum[i][i];
+            
+            return result;
+        }
+
+        /// <summary>
+        /// Soluciona el sistema de ecuaciones a partir de un amatriz LU
+        /// </summary>
+        /// <param name="luMatrix">Matriz LU</param>
+        /// <param name="b">Coeficientes b</param>
+        /// <returns>Vector de soluciones</returns>
+        private static double[] HelperSolve(double[][] luMatrix, double[] b)
+        {
+            int n = luMatrix.Length;
+            double[] x = new double[n];
+         
+            b.CopyTo(x, 0);
+            for (int i = 1; i < n; ++i)
+            {
+                double sum = x[i];
+                for (int j = 0; j < i; ++j)
+                    sum -= luMatrix[i][j] * x[j];
+                x[i] = sum;
+            }
+            
+            x[n - 1] /= luMatrix[n - 1][n - 1];
+            
+            for (int i = n - 2; i >= 0; --i)
+            {
+                double sum = x[i];
+                for (int j = i + 1; j < n; ++j)
+                    sum -= luMatrix[i][j] * x[j];
+                x[i] = sum / luMatrix[i][i];
+            }
+            
+            return x;
+        }
+    }
+}
---
+++ Galaga/Utilities/PolynomicUtilities.cs
@@ -0,0 +1,201 @@
+﻿#region GPL License
+/*  Galaga-SFML.Net: Galaga's Clon for educational purposes made with SFML.Net library
+    Copyright (C) 2015-2016  Alfredo Oltra. 
+   
+    This program comes with ABSOLUTELY NO WARRANTY.
+   
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+  
+    --------------------------------------------------------------------------------
+ 
+    You can contact the author by email: aoltra@uhurulabs.com, aoltra@gmail.com
+    or you can follow on Twitter: @aoltra
+*/
+#endregion
+
+using System;
+
+using System.Diagnostics;
+using SFML.System;
+
+namespace edu.CiclosFormativos.DAM.DI.Galaga.Utilities
+{
+    /// <summary>
+    /// Encapsula la creación de curvas polinómicas
+    /// </summary>
+    /// <remarks>
+    /// Soporta los métodos:
+    /// - Hermitte
+    /// - TODO: Bezier grado 2 y 3
+    /// </remarks>
+    public class PolynomicUtilities
+    {
+        /// <summary>
+        /// Grado de precisión para el cálculo de la longitud
+        /// </summary>
+        public static Byte Grade { get { return grade;  } set { grade = value;} }
+
+        private static Byte grade = 3;
+
+        /// <summary>
+        /// Calculo de la curva polinómica cúbica que interpola los waypoints manteniendo una determinada
+        /// pendiente en cada uno de ellos. Método de Hermitte
+        /// </summary>
+        /// <param name="points">Matriz de Nx4 (por cada fila x,y,pendX,pendY en el waypoint)</param>
+        /// <returns>
+        /// Una matriz de 2*nx5, donde n es el número de segmentos. 
+        /// Las 4 primeras columnas son los coeficientes de la función de interpolación 
+        /// (para x las filas impares y para y las pares)
+        /// El 5 es la longitud del segmento</returns>
+        public static float[,] HermiteInterpolation(float [,] points)
+        {
+            // la segunda dimensión de la matriz tiene que ser de 4 valores (x,y,pendX,pendY)
+            Debug.Assert(points.GetLongLength(1) == 4);
+            Debug.Assert(points.GetLongLength(0) > 1);
+
+            int numSegments = (int)points.GetLongLength(0) - 1, segX;
+            int numWaypoints = (int)points.GetLongLength(0);
+
+            float [,] polynomicSegments = new float[2*numSegments,5];
+
+            double[][] mat = MatrixUtilities.MatrixCreate(4 * numSegments, 4 * numSegments);
+            double[][] b = MatrixUtilities.MatrixCreate(2, 4 * numSegments);
+
+		    mat[0][0] = 1;
+		    mat[1][1] = 1;
+
+            mat[4 * numSegments - 2][4 * numSegments - 4] = 1;
+            mat[4 * numSegments - 2][4 * numSegments - 3] = 1;
+            mat[4 * numSegments - 2][4 * numSegments - 2] = 1;
+            mat[4 * numSegments - 2][4 * numSegments - 1] = 1;
+
+            mat[4 * numSegments - 1][4 * numSegments - 3] = 1;
+            mat[4 * numSegments - 1][4 * numSegments - 2] = 2.0;
+            mat[4 * numSegments - 1][4 * numSegments - 1] = 3.0;
+
+            // posición inicial
+		    b[0][0] = points[0,0]; 
+		    b[1][0] = points[0,1]; 
+
+		    // pendiente inicial
+		    b[0][1] = 0;
+		    b[1][1] = 0; 
+
+		    // Posición final
+            b[0][4 * numSegments - 2] = points[numWaypoints - 1, 0];
+            b[1][4 * numSegments - 2] = points[numWaypoints - 1, 1];
+
+		    // Pendiente final
+            b[0][4 * numSegments - 1] = 0;
+            b[1][4 * numSegments - 1] = 0;
+		
+		    for (int i = 1; i < numWaypoints - 1; i++) 
+            {
+			    int k = 4 * (i - 1) + 2;
+			    int l = 4 * (i - 1);
+
+			    mat[k][l] = 1;
+			    mat[k][l + 1] = 1;
+			    mat[k][l + 2] = 1;
+			    mat[k][l + 3] = 1;
+
+			    mat[k + 3][l + 4] = 1;
+
+			    mat[k + 1][l + 1] = 1.0;
+			    mat[k + 1][l + 2] = 2.0;
+			    mat[k + 1][l + 3] = 3.0;
+			    mat[k + 1][l + 5] = -1.0;
+
+			    mat[k + 2][l + 2] = 2.0;
+			    mat[k + 2][l + 3] = 6.0;
+			    mat[k + 2][l + 6] = -2.0;
+
+			    // Location
+			    b[0][k] = points[i,0];
+			    b[1][k] = points[i,1];
+
+			    // Pendiente
+			    b[0][k + 1]= 0;
+                b[1][k + 1] = 0;
+
+			    // pendiente
+                b[0][k + 2] = 0;
+                b[1][k + 2] = 0;
+
+			    // punto intemedio
+                b[0][k + 3] = points[i, 0];
+                b[1][k + 3] = points[i, 1];
+		    }
+
+#if DEBUG
+		    /// Impresión del sistema de ecuaciones
+            int i2b = 0;
+            for (int i = 0; i < 4 * numSegments; i++)
+            {
+                for (int i2 = 0; i2 < 4 * numSegments; i2++) 
+                    Debug.Write(mat[i][i2].ToString("F3").PadLeft(8) +"  ");
+               
+                Debug.Write("   |   ");
+                
+                Debug.WriteLine(b[0][i2b]  +  "  " + b[1][i2b]);
+                i2b++;
+            }
+#endif
+
+            // resolución
+            double[] resX = MatrixUtilities.SystemSolve(mat, b[0]);
+            double[] resY = MatrixUtilities.SystemSolve(mat, b[1]);
+
+            // almaceno los datos en los elementos de salida
+            for (int i = 0; i < numSegments; i++)
+            {
+                for (int j = 0; j < 4; j++) 
+                {
+                    polynomicSegments[2*i, j] = (float)resX[j + 4 * i];
+                    polynomicSegments[2*i + 1, j] = (float)resY[j + 4 * i];
+                }
+            }
+
+            // calculo de longitudes
+            for (int seg = 0; seg < numSegments; seg++)
+            {
+                segX = 2 * seg;
+                  
+                float incT = (1f / (float)grade), t, t2, t3;
+                float segLength = 0, xTempI, yTempI, xTempE, yTempE;
+                for (int grd = 0; grd < grade; grd++)
+                {
+                    t = grd * incT; t2 = t * t; t3 = t2 * t;
+                    xTempI = polynomicSegments[segX, 3] * t3 + polynomicSegments[segX, 2] * t2
+                        + polynomicSegments[segX, 1] * t + polynomicSegments[segX, 0];
+                    yTempI = polynomicSegments[segX + 1, 3] * t3 + polynomicSegments[segX + 1, 2] * t2
+                        + polynomicSegments[segX + 1, 1] * t + polynomicSegments[segX + 1, 0];
+
+                    t = (grd + 1) * incT; t2 = t * t; t3 = t2 * t;
+                    xTempE = polynomicSegments[segX, 3] * t3 + polynomicSegments[segX, 2] * t2
+                        + polynomicSegments[segX, 1] * t + polynomicSegments[segX, 0];
+                    yTempE = polynomicSegments[segX + 1, 3] * t3 + polynomicSegments[segX + 1, 2] * t2
+                        + polynomicSegments[segX + 1, 1] * t + polynomicSegments[segX + 1, 0];
+
+                    segLength += Utilities.VectorUtilities.VectorLength(new Vector2f(xTempI - xTempE, yTempI - yTempE));
+
+                }
+
+                polynomicSegments[segX, 4] = polynomicSegments[segX + 1, 4] = segLength;
+            }
+
+            return polynomicSegments;
+        }
+    }
+}
